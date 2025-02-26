937ce9dc3aad3673567ab518055a4044c0940b83
Pavel Václavek <pavel.vaclavek88@gmail.com>
2025-02-26 11:35:27 +0100

    Added world and room creation from file

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,56 @@
+import java.util.ArrayList;
+
 public class Room {
+
+    private final int ID;
+    private String name;
+    private ArrayList<Character> characters;
+    private ArrayList<Integer> connections;
+
+    public Room(String roomString) {
+
+        connections = new ArrayList<>();
+        characters = new ArrayList<>();
+
+        String[] tokens = roomString.split(";");
+        ID = Integer.parseInt(tokens[0]);
+        name = tokens[1];
+
+        String[] connectionString = tokens[2].split(",");
+        for (int i = 0; i < connectionString.length; i++) {
+            try {
+                connections.add(Integer.parseInt(connectionString[i]));
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Character> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(ArrayList<Character> characters) {
+        this.characters = characters;
+    }
+
+    public ArrayList<Integer> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(ArrayList<Integer> connections) {
+        this.connections = connections;
+    }
 }
--- src/World.java
+++ src/World.java
@@ -1,2 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class World {
+
+    private HashMap<Integer, Room> rooms;
+    private int startingRoom;
+    private int currentRoom;
+
+    public World(String filename) {
+        rooms = new HashMap<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+
+            while((br.readLine()) != null) {
+                String line = br.readLine();
+                Room room = new Room(line);
+                rooms.put(room.getID(), room);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        startingRoom = 1;
+        currentRoom = startingRoom;
+
+    }
 }
