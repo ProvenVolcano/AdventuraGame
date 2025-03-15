154f4a0fdf243298987afc0f365320a996b663b1
Pavel Václavek <pavel.vaclavek88@gmail.com>
2025-03-15 12:05:18 +0100

    Added loading Interactables from file + separated constructor in Room

--- src/World/Room.java
+++ src/World/Room.java
@@ -14,7 +14,6 @@ public class Room {
     private ArrayList<Integer> connections;
     private HashMap<String, Item> items;
     private HashMap<String, Interactable> interactables;
-    private String message;
 
     public Room(String roomString, Player player) {
 
@@ -27,6 +26,16 @@ public class Room {
         ID = Integer.parseInt(tokens[0]);
         name = tokens[1];
 
+        createConnections(tokens);
+        createItems(tokens, player);
+        createInteractables(tokens, player);
+    }
+
+    private void createConnections(String[] tokens) {
+        if(tokens[2].isBlank()){
+            return;
+        }
+
         String[] connectionString = tokens[2].split(",");
         for (int i = 0; i < connectionString.length; i++) {
             try {
@@ -35,19 +44,38 @@ public class Room {
                 System.out.println(e.getMessage());
             }
         }
+    }
 
-        try {
-            String[] itemString = tokens[3].split(",");
-            for (int i = 0; i < itemString.length; i++) {
-                try {
-                    items.put(itemString[i], Item.factory(itemString[i], player));
-                } catch (NumberFormatException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        } catch (Exception e) {
-            //System.out.println(e.getMessage());
+    private void createItems(String[] tokens, Player player) {
+        if(tokens[3].isBlank()){
+            return;
         }
+
+        String[] itemString = tokens[3].split(",");
+        for (int i = 0; i < itemString.length; i++) {
+            try {
+                items.put(itemString[i], Item.factory(itemString[i], player));
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+    private void createInteractables(String[] tokens, Player player) {
+        if(tokens[4].isBlank()){
+            return;
+        }
+
+        String[] interactableString = tokens[4].split(",");
+        for (int i = 0; i < interactableString.length; i++) {
+            try {
+                interactables.put(interactableString[i], Interactable.factory(interactableString[i], player));
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
     }
 
     public HashMap<String, Character> getCharacters() {
